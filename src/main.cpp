478a25e669da6ffa03b3160ef7eae7027cf40e3a
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2025-04-15 09:52:59 -0400

    Relativement stable

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@
 #include <AiEsp32RotaryEncoder.h>
 
 // ----- PID -----
-float setpoint = 3.0;       // Maintenant variable dynamique
-#define DT 10               // Intervalle en ms
-#define AFFICHAGE (1000 / DT)
-#define K0 6.65
+float setPoint_deg = 3.0;       // Maintenant variable dynamique
+#define DT_ms 10               // Intervalle en ms
+#define K0 7.2
 #define KP 0.01
+#define KI 0.0
+#define KD 0.0
 
-int compteur = 0;
+int compteur_affichage = 0;
 
 // ----- MPU -----
 Adafruit_MPU6050 mpu;
@@ -64,54 +65,63 @@ void setup() {
   // ENCODER INIT
   rotaryEncoder.begin();
   rotaryEncoder.setup(readEncoderISR);
-  rotaryEncoder.setBoundaries(-45, 45, false); // plage du setpoint
-  rotaryEncoder.setEncoderValue((int)setpoint); // valeur initiale
+  rotaryEncoder.setBoundaries(-45, 45, false); // plage du setPoint_deg
+  rotaryEncoder.setEncoderValue((int)setPoint_deg); // valeur initiale
   rotaryEncoder.disableAcceleration();
 
   Serial.println("Setup terminé");
-  ledcWrite(PWM_CHANNEL, 200.655); // PWM à 0 au démarrage
+  ledcWrite(PWM_CHANNEL, 200.655); // PWM à 4.9% au démarrage
   delay(5000); // Laisser le temps au moteur de se stabiliser
 }
 
 // ----- LOOP -----
 void loop() {
-  static unsigned long lastTime = 0;
-  unsigned long currentTime = millis();
+  static unsigned long lastTime_ms = 0;
+  unsigned long currentTime_ms = millis();
 
-  if (currentTime - lastTime >= DT) {
-    lastTime = currentTime;
-    compteur++;
+  if (currentTime_ms - lastTime_ms >= DT_ms) {
+    lastTime_ms = currentTime_ms;
+    compteur_affichage++;
 
-    // 1. Lire le setpoint de l’encodeur
+    // 1. Lire le setPoint_deg de l’encodeur
     if (rotaryEncoder.encoderChanged()) {
-      setpoint = (float)rotaryEncoder.readEncoder(); // en degrés
-      Serial.print("Setpoint modifié: ");
-      Serial.println(setpoint);
+      setPoint_deg = (float)rotaryEncoder.readEncoder(); // en degrés
+      Serial.print("SetPoint modifié: ");
+      Serial.println(setPoint_deg);
     }
 
     // 2. Lire angle du capteur
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
-    float angle = -atan2(a.acceleration.x, a.acceleration.z) * 180.0 / PI;
+    float angle_deg = -atan2(a.acceleration.x, a.acceleration.z) * 180.0 / PI;
 
     // 3. Contrôle P
-    float erreur = setpoint - angle;
-    float sortie = K0 + KP * erreur;
-    sortie = sortie > 6.9 ? 6.9 : sortie; // Limiter la sortie à 6.9 pour éviter le dépassement
-    sortie = sortie < 6.4 ? 6.4 : sortie; // Limiter la sortie à 0.0 pour éviter le dépassement
+    float erreur = setPoint_deg - angle_deg;
+    float integrale_erreur = integrale_erreur + (erreur * (DT_ms / 1000)); // Erreur intégrée sur 10ms
 
-    int pwmValue = (int)((sortie / 100.0) * 4095.0);
+    float sortie = K0 + KP * erreur;
+    
+    sortie = sortie > 7.3 ? 7.3 : sortie; // Limiter la sortie à 6.9 pour éviter le dépassement
+    sortie = sortie < 7 ? 7 : sortie; // Limiter la sortie à 0.0 pour éviter le dépassement
+
+    int pwmValue = (int)((sortie / 100.0) * 4095.0); // Convertir en valeur PWM (0-4095 pour 12 bits)
     ledcWrite(PWM_CHANNEL, pwmValue);
 
     // 4. Affichage toutes les x itérations
-    if (compteur >= AFFICHAGE) {
-      compteur = 0;
+    if (compteur_affichage >= 10) {
+      compteur_affichage = 0;
       Serial.print(">");
-      Serial.print("var1: ");
-      Serial.print(angle);
+      Serial.print("angle: ");
+      Serial.print(angle_deg);
       Serial.print(",");
-      Serial.print("var2: ");
-      Serial.print(setpoint);
+      Serial.print("setPoint: ");
+      Serial.print(setPoint_deg);
+      Serial.print(",");
+      Serial.print("erreur: ");
+      Serial.print(erreur);
+      Serial.print(",");
+      Serial.print("sortie: ");
+      Serial.print(sortie);
       Serial.println("");
     }
   }
